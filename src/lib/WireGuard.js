2d68707e0599a5008f8cd91a13ee65dd26a21ed1
Emmanuel Alves <manel.pb@gmail.com>
2025-01-30 13:02:04 -0500

    feat(WireGuard): add optional MTU configuration to WireGuard server settings

--- src/lib/WireGuard.js
+++ src/lib/WireGuard.js
@@ -109,6 +109,7 @@ PreUp = ${WG_PRE_UP}
 PostUp = ${WG_POST_UP}
 PreDown = ${WG_PRE_DOWN}
 PostDown = ${WG_POST_DOWN}
+${WG_MTU ? `MTU = ${WG_MTU}\n` : ''}
 `;
 
     for (const [clientId, client] of Object.entries(config.clients)) {
